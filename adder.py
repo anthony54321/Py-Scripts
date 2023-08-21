0f8fa4aa69c7879de38a3f4693dd1544dccc8803
anthony54321 <101435593+anthony54321@users.noreply.github.com>
2023-08-21 12:37:04 -0400

    Update and rename test.py to adder.py

---
+++ adder.py
@@ -0,0 +1,24 @@
+#create an integer variable for x
+x = int(0)
+#create an integer variable for y
+y= int(0)
+#create an integer variable for sum
+sum = 0
+
+#ask the user "X: " and put answer in x
+x = input("X: ")
+
+#ask the user "Y: " and put answer in y
+y = input("Y: ")
+
+#convert x to integer
+x = int(x)
+#convert y to integer
+y = int(y)
+
+#put x + y in sum
+sum = x + y
+
+#tell your user "answer is " sum
+print ("Sum is ")
+print (sum)
